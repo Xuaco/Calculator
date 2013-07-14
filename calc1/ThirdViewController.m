7d42dcc66579210f035728e311171e819e942402
Joaquin Arias <joaquin.arias@imdea.org>
2013-07-14 11:26:30 +0200

    cambios en otro sitio

--- calc1/ThirdViewController.m
+++ calc1/ThirdViewController.m
@@ -8,7 +8,7 @@
 // comentarios nuevos desde master
 
 // ver gestos en http://developer.apple.com/library/ios/#samplecode/SimpleGestureRecognizers/Listings/SimpleGestureRecognizers_APLGestureRecognizerViewController_m.html#//apple_ref/doc/uid/DTS40009460-SimpleGestureRecognizers_APLGestureRecognizerViewController_m-DontLinkElementID_5
-
+// aqui no daria problemas
 
 #import "ThirdViewController.h"
 #import "SecondViewController.h"
